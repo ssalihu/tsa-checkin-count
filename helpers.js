a19dda249b65cdc135f1b549f6ce5914d6941178
Safir Salihu <msafirs@gmail.com>
2021-02-05 13:56:51 -0600

    Support for 2021

--- helpers.js
+++ helpers.js
@@ -16,11 +16,12 @@ function extractListingsFromHTML (html) {
     // let closing = $(el).children('.views-field-field-vacancy-deadline').first().text().trim();
     let date = $(el).children().first().text().trim();
     let current = $(el).children().first().next().text().trim();
-    let previous = $(el).children().next().next().text().trim();
+    let count_2020 = $(el).children().first().next().next().text().trim();
+    let count_2019 = $(el).children().next().next().next().text().trim();
     //closing = moment(closing.slice(0, closing.indexOf('-') - 1), 'DD/MM/YYYY').toISOString();
     d =  new Date((date)).getTime();
     if(d) {
-       vacancies.push({date, current, previous});
+       vacancies.push({date, current, count_2020, count_2019});
     }
   });
   map[key] = vacancies;
